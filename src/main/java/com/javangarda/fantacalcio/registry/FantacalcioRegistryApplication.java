75285467653e4ce2a19cb19b2bc019907258bf29
acichon <acichon89@gmail.com>
2017-05-18 23:21:51 +0200

    first commit

---
+++ src/main/java/com/javangarda/fantacalcio/registry/FantacalcioRegistryApplication.java
@@ -0,0 +1,14 @@
+package com.javangarda.fantacalcio.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class FantacalcioRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantacalcioRegistryApplication.class, args);
+	}
+}
---
+++ src/test/java/com/javangarda/fantacalcio/registry/FantacalcioRegistryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.javangarda.fantacalcio.registry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FantacalcioRegistryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
